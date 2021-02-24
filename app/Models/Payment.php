859f87f59f6af3311a29aa8e2d4d065905ca1a66
David Cancio <cancioalmaraz@gmail.com>
2021-02-24 07:30:53 -0400

    Adding relationship between payment - client

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,18 @@ use App\Models\BaseModel;
 
 class Payment extends BaseModel
 {
-    //
+    public function clients(){
+        return $this->belongsToMany(Client::class, 'clients_payments', 'payment_id', 'client_id');
+    }
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+
+        if (!is_null($this->clients)) {
+            $array['clients'] = $this->clients()->get();
+        }
+
+        return $array;
+    }
 }
